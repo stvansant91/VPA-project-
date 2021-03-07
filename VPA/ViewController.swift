6513631fc0c8d7138289c30875ed1037907afb48
stvansant91 <80180319+stvansant91@users.noreply.github.com>
2021-03-06 17:22:55 -0800

    Updated the Functionality and desgin
    Changed background and made the randomization work

--- VPA/ViewController.swift
+++ VPA/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+  
+    @IBOutlet weak var randomTextLabel: UILabel!
+    
+    var randomText = 0.0
+    
+    let randomLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "u", "r", "s", "t", "u", "v", "x", "y", "z",]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func randomButtonPressed(_ sender: UIButton) {
+        randomTextLabel.text = randomLetters[Int.random(in: 0...25)]
+    }
+    
 }
 
